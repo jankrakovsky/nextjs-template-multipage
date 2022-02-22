3ea859d8f55a90f0dad4e71791ba32f74a025706
afneybeyb <afneybeyb@gmail.com>
2022-02-22 11:27:23 +0100

    Home-Start is responsive

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,17 @@ import marginaliaProductiveWork from "../src/img/marginalia-productive-work.svg"
 export default function Home() {
 	return (
 		<div className="Home-Start bg-primary-flamingo h-screen">
-			<div className="w-full lg:w-10/12 h-full m-auto grid grid-cols-2 place-content-center">
-				<div className="flex flex-col justify-center text-white">
+			<div className="w-full xl:w-10/12 h-full m-auto p-2 grid lg:grid-cols-2 justify-start sm:justify-center lg:justify-start content-between md:content-center sm:items-center">
+				<div className="text-white m-auto">
 					<h5 className="mb-[26px]">Delectus instructior</h5>
-					<h1>Vide legimus <br />consectetuer</h1>
+					<h1 className="text-[56px] sm:text-[72px] leading-[60px] sm:leading-[80px]">Vide legimus <br />consectetuer</h1>
 					<div className="EmailForm">
-						<input className="mt-[33px]" type="email" name="emailInput" id="emailInput" placeholder="Email" />
-						<button className="Dark_Primary ml-[30px]" type="submit">Submit</button>
+						<input className="w-full sm:w-[270px] mt-[33px] mb-[16px]" type="email" name="emailInput" id="emailInput" placeholder="Email" />
+						<button className="Dark_Primary ml-0 sm:ml-[30px]" type="submit">Submit</button>
 					</div>
 				</div>
-				<Image src={marginaliaProductiveWork} alt="image" width={659} height={644} />
-			</div >
+				<Image src={marginaliaProductiveWork} alt="" />
+			</div>
 		</div>
 	)
 }
