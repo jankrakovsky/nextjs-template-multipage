110bd109631cb1c6b826564b94e89cf641326a49
afneybeyb <afneybeyb@gmail.com>
2022-03-01 04:41:37 +0100

    created basic Navbar as a placeholder

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+	return (
+		<nav className="Navbar grid grid-cols-2">
+			<div className="Navbar-left flex justify-between bg-blue-300">
+				<div>Test</div>
+				<div>Test</div>
+			</div>
+			<div className="Navbar-right flex justify-between bg-red-300">
+				<div>Test</div>
+				<div>Test</div>
+			</div>
+		</nav>
+	);
+}
+
+export default Navbar;
+
