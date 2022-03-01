f0050490d62dbd6e80874f3931a5b2501cb5262e
afneybeyb <afneybeyb@gmail.com>
2022-03-01 05:09:47 +0100

    written proper Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,17 @@
 const Navbar = () => {
 	return (
-		<nav className="Navbar grid grid-cols-2">
-			<div className="Navbar-left flex justify-between bg-blue-300">
-				<div>Test</div>
-				<div>Test</div>
-			</div>
-			<div className="Navbar-right flex justify-between bg-red-300">
-				<div>Test</div>
-				<div>Test</div>
-			</div>
-		</nav>
+		<div className="Navbar text-white bg-primary-flamingo">
+			<nav className="Navbar-nav w-full h-full m-auto xl:w-10/12 px-4 py-2 grid grid-cols-2 gap-12 items-center">
+				<div className="Navbar-left flex justify-between items-center">
+					<h3>LOGO</h3>
+					<p className="Text">Portfolio</p>
+				</div>
+				<div className="Navbar-right flex justify-between items-center">
+					<p className="Text">Contact</p>
+					<div className="Text px-12 py-3 border-[1px] border-white border-solid">Sign up</div>
+				</div>
+			</nav>
+		</div>
 	);
 }
 
