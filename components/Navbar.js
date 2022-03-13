04c536d2a5be5e0953426c48cc9fc5803a4aa1a1
afneybeyb <afneybeyb@gmail.com>
2022-03-13 16:40:41 +0100

    using Link in Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,26 @@
+import Link from 'next/link';
+
 const Navbar = () => {
 	return (
 		<div className="Navbar text-white bg-primary-flamingo">
 			<nav className="Navbar-nav w-full h-full m-auto xl:w-10/12 px-4 py-2 grid grid-cols-3 gap-12 justify-between items-center">
 				<div className="Navbar-left items-center">
-					<h3>LOGO</h3>
+					<Link href="/">
+						<h3 className="cursor-pointer">LOGO</h3>
+					</Link>
 				</div>
 				<div className="flex justify-center items-center gap-4">
-					<p className="Text">Portfolio</p>
-					<p className="Text">Contact</p>
+					<Link href="/portfolio">
+						<p className="Text cursor-pointer">Portfolio</p>
+					</Link>
+					<Link href="/contact">
+						<p className="Text cursor-pointer">Contact</p>
+					</Link>
 				</div>
 				<div className="Navbar-right flex justify-end items-center">
-					<div className="Text px-12 py-3 border-[1px] border-white border-solid">Sign up</div>
+					<Link href="/">
+						<div className="Text px-12 py-3 border-[1px] border-white border-solid cursor-pointer">Sign up</div>
+					</Link>
 				</div>
 			</nav>
 		</div>
