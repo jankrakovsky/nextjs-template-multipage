e2870846a0bcad8ff820b9ebb437b145aa8944d6
afneybeyb <afneybeyb@gmail.com>
2022-03-13 14:02:24 +0100

    rewritten Navbar so it is better prepared to burger menu when on phone

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,15 @@
 const Navbar = () => {
 	return (
 		<div className="Navbar text-white bg-primary-flamingo">
-			<nav className="Navbar-nav w-full h-full m-auto xl:w-10/12 px-4 py-2 grid grid-cols-2 gap-12 items-center">
-				<div className="Navbar-left flex justify-between items-center">
+			<nav className="Navbar-nav w-full h-full m-auto xl:w-10/12 px-4 py-2 grid grid-cols-3 gap-12 justify-between items-center">
+				<div className="Navbar-left items-center">
 					<h3>LOGO</h3>
-					<p className="Text">Portfolio</p>
 				</div>
-				<div className="Navbar-right flex justify-between items-center">
+				<div className="flex justify-center items-center gap-4">
+					<p className="Text">Portfolio</p>
 					<p className="Text">Contact</p>
+				</div>
+				<div className="Navbar-right flex justify-end items-center">
 					<div className="Text px-12 py-3 border-[1px] border-white border-solid">Sign up</div>
 				</div>
 			</nav>
