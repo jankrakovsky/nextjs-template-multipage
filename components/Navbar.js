c21cec6149a859f79a7885b0a9c5469e9be37d80
afneybeyb <afneybeyb@gmail.com>
2022-03-13 17:39:49 +0100

    prepared Navbar for burger menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,19 @@
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Navbar = () => {
+	const [showMenu, setShowMenu] = useState(true);
+
 	return (
 		<div className="Navbar text-white bg-primary-flamingo">
-			<nav className="Navbar-nav w-full h-full m-auto xl:w-10/12 px-4 py-2 grid grid-cols-3 gap-12 justify-between items-center">
-				<div className="Navbar-left items-center">
+			<nav className="Navbar-nav w-full h-full m-auto xl:w-10/12 px-4 py-2 grid grid-flow-row sm:grid-flow-col gap-12 justify-between items-center">
+				<div className="Navbar-left w-[90vw] sm:w-auto relative items-center">
 					<Link href="/">
 						<h3 className="cursor-pointer">LOGO</h3>
 					</Link>
+					<div className="Burger-menu w-[35px] h-[35px] right-[0px] top-[10px] absolute sm:hidden bg-black cursor-pointer"></div>
 				</div>
-				<div className="flex justify-center items-center gap-4">
+				<div className="flex flex-col sm:flex-row justify-center items-end sm:items-center gap-4">
 					<Link href="/portfolio">
 						<p className="Text cursor-pointer">Portfolio</p>
 					</Link>
@@ -22,6 +26,7 @@ const Navbar = () => {
 						<div className="Text px-12 py-3 border-[1px] border-white border-solid cursor-pointer">Sign up</div>
 					</Link>
 				</div>
+
 			</nav>
 		</div>
 	);
