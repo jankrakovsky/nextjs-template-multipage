9cee7fbd9122cbc774ef307e15b473ace253ae13
afneybeyb <afneybeyb@gmail.com>
2022-03-13 17:00:58 +0100

    sm: TwoColumns now spaces content around, not between

--- components/TwoColumns.js
+++ components/TwoColumns.js
@@ -1,6 +1,6 @@
 const TwoColumns = ({ children }) => {
 	return (
-		<div className="TwoColumns w-full xl:w-10/12 h-full m-auto p-2 grid lg:grid-cols-2 justify-start sm:justify-center lg:justify-start content-between md:content-center sm:items-center">
+		<div className="TwoColumns w-full xl:w-10/12 h-full m-auto p-2 grid lg:grid-cols-2 justify-start sm:justify-center lg:justify-start content-around md:content-center sm:items-center">
 			{children}
 		</div>
 	);
