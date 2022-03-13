27ec07daa6f4ee0f04d37554690c8dbc756ff77e
afneybeyb <afneybeyb@gmail.com>
2022-03-13 17:01:12 +0100

    Features are now responsive

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import docs from "../public/img/icons/docs.svg";
 export default function Home() {
 	return (
 		<div className="Home">
-			<div className="Home-start min-h-[900px] h-screen bg-primary-flamingo text-white">
+			<div className="Home-start min-h-[900px] h-screen py-2 bg-primary-flamingo text-white">
 				<TwoColumns>
 					<div className="m-auto">
 						<h5 className="mb-[26px]">Delectus instructior</h5>
@@ -23,13 +23,13 @@ export default function Home() {
 				</TwoColumns>
 			</div>
 
-			<div className="Home-features min-h-[900px] h-screen bg-white grid content-center">
-				<div className="m-auto w-min text-center">
+			<div className="Home-features min-h-[900px] sm:h-screen py-2 bg-white grid content-center">
+				<div className="m-auto sm:w-min text-center">
 					<h5 className=" text-primary-red">Nominavi suavitate</h5>
-					<h2 className="w-max">Offendit persecuti</h2>
+					<h2 className="sm:w-max">Offendit persecuti</h2>
 					<p className="Text_Big mt-4">Eu pro ludus dignissim. Eos debet molestiae constituto ne, quo consul interesset ei, cotidieque theophrastus ea his.</p>
 				</div>
-				<div className="mt-6 grid grid-cols-3">
+				<div className="mt-6 grid grid-cols-1 sm:grid-cols-3">
 					<IconSquare num="01" img={stocks} title="Possim" text="An animal assentior nam, sed qualisque deterruisset ea qui melius erroribus" />
 					<IconSquare num="02" img={bag} title="Vivendo" text="Assum ubique quodsi sea ad partem vocent vel, ius id viris utinam" />
 					<IconSquare num="03" img={docs} title="Deterruisset" text="Nam natum volutpat eu natum elitr vel te id qui purto dicit" />
