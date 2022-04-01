d18b950829e3863369a47f6e62b00bb57a668be2
afneybeyb <afneybeyb@gmail.com>
2022-04-01 15:37:16 +0200

    changed absolute position of the text div

--- components/PortfolioPic.js
+++ components/PortfolioPic.js
@@ -4,7 +4,7 @@ const PortfolioPic = (props) => {
 	return (
 		<div className="w-fit h-fit mb-12 relative flex flex-row justify-center">
 			<Image src={props.img} alt="" />
-			<div className="w-11/12 absolute bottom-[-2.5rem] p-2 bg-white border-2 border-solid border-[#E5E5E5] box-border shadow-sm rounded-[3px]">
+			<div className="w-11/12 absolute bottom-[-2rem] p-2 bg-white border-2 border-solid border-[#E5E5E5] box-border shadow-sm rounded-[3px]">
 				<h4>{props.title}</h4>
 				<p className="Text_Gray">{props.text}</p>
 			</div>
