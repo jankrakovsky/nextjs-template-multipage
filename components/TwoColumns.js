022dff447c8f95fabf4d0b8f7b72c96a3247fb0f
afneybeyb <afneybeyb@gmail.com>
2022-04-01 15:36:32 +0200

    added animation to TwoColumns when resizing

--- components/TwoColumns.js
+++ components/TwoColumns.js
@@ -1,6 +1,6 @@
 const TwoColumns = ({ children }) => {
 	return (
-		<div className="TwoColumns w-full xl:w-10/12 h-full m-auto p-2 grid lg:grid-cols-2 gap-6 justify-start sm:justify-center lg:justify-start content-around md:content-center sm:items-center">
+		<div className="TwoColumns w-full xl:w-10/12 h-full m-auto p-2 grid lg:grid-cols-2 gap-6 justify-start sm:justify-center lg:justify-start content-around md:content-center sm:items-center transition-all">
 			{children}
 		</div>
 	);
