b179869b1aa13c3843762b82d9ed383f2bc747cb
afneybeyb <afneybeyb@gmail.com>
2022-07-15 09:12:08 +0200

    fix: added passHref to pass the href to the component inside

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
 		<div className="Navbar text-white bg-primary-flamingo">
 			<nav className="Navbar-nav w-full h-full m-auto xl:w-10/12 py-2 grid grid-flow-row sm:grid-flow-col gap-12 justify-between items-center">
 				<div className="Navbar-left w-[100vw] px-6 sm:w-auto relative items-center">
-					<Link href="/">
+					<Link href="/" passHref>
 						<h3 className="cursor-pointer w-fit">LOGO</h3>
 					</Link>
 
@@ -25,15 +25,15 @@ const Navbar = () => {
 					</div>
 				</div>
 				<div className={`Navbar-center px-6 ${showMenu} sm:flex flex-col sm:flex-row justify-center items-end sm:items-center gap-4`}>
-					<Link href="/portfolio">
+					<Link href="/portfolio" passHref>
 						<p className="Text cursor-pointer hover:border-b-2 hover:border-solid hover:border-white">Portfolio</p>
 					</Link>
-					<Link href="/contact">
+					<Link href="/contact" passHref>
 						<p className="Text cursor-pointer hover:border-b-2 hover:border-solid hover:border-white">Contact</p>
 					</Link>
 				</div>
 				<div className={`Navbar-right px-6 ${showMenu} sm:flex justify-end items-center`}>
-					<Link href="/">
+					<Link href="/" passHref>
 						<div className="Text px-12 py-3 border-[1px] border-white border-solid cursor-pointer">Sign up</div>
 					</Link>
 				</div>
